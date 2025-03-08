6586f562676d74079dea801d5419b4e21604a254
Hamzenium <hamza.sohail29@gmail.com>
2025-03-08 05:40:18 -0500

    Removed APIs

--- api/backend/services/barber.py
+++ api/backend/services/barber.py
@@ -5,10 +5,3 @@ from backend import mongo, bcrypt
 barber_bp = Blueprint("barbers", __name__)
 
 # Get Available Slots for a Barber
-@barber_bp.route("/<barber_id>/slots", methods=["GET"])
-def get_available_slots(barber_id):
-    barber = mongo.barbers.find_one({"_id": ObjectId(barber_id)})
-    if not barber:
-        return jsonify({"error": "Barber not found"}), 404
-    available_slots = [slot for slot in barber["available_slots"] if not slot["booked"]]
-    return jsonify(available_slots)
